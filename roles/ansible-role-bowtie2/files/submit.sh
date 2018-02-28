50dd137e85079aa301b5944639e0de2ad82d69f8
Tanner Dowhy <tad638@mail.usask.ca>
2018-02-28 01:55:29 -0600

    debug awx

--- roles/ansible-role-bowtie2/files/submit.sh
+++ roles/ansible-role-bowtie2/files/submit.sh
@@ -4,8 +4,6 @@ module load bowtie2;
 
 bowtie2-build "$2" "$1"/bowtie2/contaminants_indx;
 
-for file in "$1"/merging/output/*extendedFrags.fastq;
-    do sbatch "$1"/bowtie2/bowtie2.sh "$file" "$1";
-done;
+sh "$1"/bowtie2/_submit.sh "$1"
 
 exit 0;
