7ef2c2d337281728c6408fd9470cd3ff0a5ae356
Tanner Dowhy <tad638@mail.usask.ca>
2018-02-27 23:14:26 -0600

    fix submit script path

--- roles/ansible-role-bowtie2/files/submit.sh
+++ roles/ansible-role-bowtie2/files/submit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+module load bowtie2
+
 bowtie2-build "$2" "$1"/bowtie2/contaminants_indx
 
-for file in "$1"/merging/out/*extendedFrags.fastq.;
+for file in "$1"/merging/output/*extendedFrags.fastq.;
     do sbatch "$1"/bowtie2/bowtie2.sh "$file" "$1";
 done;
 
