628baf2de0a7e4680c54b1442963158e4d17f60e
Tanner Dowhy <tad638@mail.usask.ca>
2018-02-27 23:20:57 -0600

    found the rogue period that was breaking everything

--- roles/ansible-role-bowtie2/files/submit.sh
+++ roles/ansible-role-bowtie2/files/submit.sh
@@ -4,7 +4,7 @@ module load bowtie2
 
 bowtie2-build "$2" "$1"/bowtie2/contaminants_indx
 
-for file in "$1"/merging/output/*extendedFrags.fastq.;
+for file in "$1"/merging/output/*extendedFrags.fastq;
     do sbatch "$1"/bowtie2/bowtie2.sh "$file" "$1";
 done;
 
