91831ea23532f0d9d26b8224ba7c16e0eb11e791
Tanner Dowhy <tad638@mail.usask.ca>
2018-03-09 01:14:30 -0600

    fix dada path issue

--- roles/ansible-role-dada2-run/files/submit.sh
+++ roles/ansible-role-dada2-run/files/submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cat "$1"/data/bowtie2/out/*.fastq > "$1"/data/dada/combined.fastq;
-sh "$1"/software/bbmap/readlength.sh in="$1"/data/dada/combined.fastq out="$1"/data/dada/hist.txt bin=1;
-len="$(python "$1"/data/dada/read_length.py -f "$1"/data/dada/hist.txt'i);
+cat "$1"/bowtie2/out/*.fastq > "$1"/dada/combined.fastq;
+sh "$1"/software/bbmap/readlength.sh in="$1"/dada/combined.fastq out="$1"/dada/hist.txt bin=1;
+len="$(python "$1"/dada/read_length.py -f "$1"/dada/hist.txt'i);
 
 sh _submit.sh "$1" "$2" "$len";
 
