506a5993195d7ca560bfde8357825848bda417c8
Tanner Dowhy <tad638@mail.usask.ca>
2018-03-09 01:09:33 -0600

    fix slurm output

--- roles/ansible-role-bowtie2/files/_submit.sh
+++ roles/ansible-role-bowtie2/files/_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for file in "$1"/merging/output/*extendedFrags.fastq;
-    do sbatch --account="$3" --dependency=singleton "$1"/bowtie2/bowtie2.sh "$file" "$1";
+    do sbatch --output="$1"/bowtie2 --account="$3" --dependency=singleton "$1"/bowtie2/bowtie2.sh "$file" "$1";
 done;
 
 exit 0;
--- roles/ansible-role-merging/files/submit.sh
+++ roles/ansible-role-merging/files/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for file in "$1"/primer_removal/*_R1*.fastq.gz;
-    do sbatch --account="$2" --dependency=singleton "$1"/merging/merge.sh "$file" "$1";
+    do sbatch --output=/dev/null --account="$2" --dependency=singleton "$1"/merging/merge.sh "$file" "$1";
 done;
 
 exit 0;
