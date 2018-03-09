8dac1c37843da6c0556471020f5c1580f67230d0
Tanner Dowhy <tad638@mail.usask.ca>
2018-03-09 01:35:59 -0600

    add queue check

--- roles/ansible-role-dada2-run/files/submit.sh
+++ roles/ansible-role-dada2-run/files/submit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sh queue.sh "$2"
+
 cat "$1"/bowtie2/out/*.fastq > "$1"/dada/combined.fastq;
 sh "$1"/software/bbmap/readlength.sh in="$1"/dada/combined.fastq out="$1"/dada/hist.txt bin=1;
 len="$(python "$1"/dada/read_length.py -f "$1"/dada/hist.txt'i);
