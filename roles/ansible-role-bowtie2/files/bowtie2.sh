c4f8d562a845ace4045281e66121357f6b41c12f
TannerDowhy <tad638@mail.usask.ca>
2018-04-04 00:24:58 -0600

    Update bowtie2.sh

--- roles/ansible-role-bowtie2/files/bowtie2.sh
+++ roles/ansible-role-bowtie2/files/bowtie2.sh
@@ -7,9 +7,9 @@
 inp="$1";
 file=${inp##*/};
 base=${file%*.extended*};
-path=$(dirname "${1}")
+path=$(dirname "${1}");
 
-module load bowtie2
-bowtie2 -x "$2"/bowtie2/contaminants_indx "$1" --un "$2"/bowtie2/out/phix_"$base".fastq > "$2"/bowtie2/sam/"$base".out
+module load bowtie2;
+bowtie2 -x "$2"/bowtie2/contaminants_indx "$1" --un "$2"/bowtie2/out/phix_"$base".fastq > "$2"/bowtie2/sam/"$base".out;
 
 exit 0;
