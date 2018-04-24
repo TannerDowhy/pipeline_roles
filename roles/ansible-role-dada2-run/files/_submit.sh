51b6da8abcadf2293c11ccb35d6906ee1c6ac328
Tanner Dowhy <tad638@mail.usask.ca>
2018-04-24 12:23:47 -0600

    fix

--- roles/ansible-role-dada2-run/files/_submit.sh
+++ roles/ansible-role-dada2-run/files/_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sbatch --account="$2" --output="$1"/dada/sample_inference.out "$1"/dada/dada_sample_inference.sh "$1"/bowtie2/out "$3" "$1"/dada/plot.png "$1"/dada/seq_tab.csv "$1"/dada/seq_tab.rds;
+sbatch --output="$1"/dada/sample_inference.out "$1"/dada/dada_sample_inference.sh "$1"/bowtie2/out "$3" "$1"/dada/plot.png "$1"/dada/seq_tab.csv "$1"/dada/seq_tab.rds;
 
-sbatch --account="$2" --output="$1"/dada/chimera_taxonomy.out --dependency=singleton "$1"/dada/dada_chimera_taxonomy.sh "$1"/dada/seq_tab.rds "$1"/dada/seqtab_final.csv "$1"/dada/tax_final.csv "$1"/dada/seqtab_final.rds "$1"/dada/tax_final.rds;
+sbatch --output="$1"/dada/chimera_taxonomy.out --dependency=singleton "$1"/dada/dada_chimera_taxonomy.sh "$1"/dada/seq_tab.rds "$1"/dada/seqtab_final.csv "$1"/dada/tax_final.csv "$1"/dada/seqtab_final.rds "$1"/dada/tax_final.rds;
 
 exit 0;
