a37da90bcecd313c2715af95d1cb1962fdbaa691
Tanner Dowhy <tad638@mail.usask.ca>
2018-04-24 12:43:52 -0600

    fix

--- roles/ansible-role-dada2-run/files/submit.sh
+++ roles/ansible-role-dada2-run/files/submit.sh
@@ -19,6 +19,10 @@
 
 len=`python $1/dada/read_length.py -f $1/dada/hist.txt`;
 
-sh _submit.sh "$1" "$2" "$len";
+sbatch --account="$2" --output="$1"/dada/sample_inference.out "$1"/dada/dada_sample_inference.sh "$1"/bowtie2/out "$len" "$1"/dada/plot.png "$1"/dada/seq_tab.csv "$1"/dada/seq_tab.rds;
+
+sbatch --account="$2" --output="$1"/dada/chimera_taxonomy.out --dependency=singleton "$1"/dada/dada_chimera_taxonomy.sh "$1"/dada/seq_tab.rds "$1"/dada/seqtab_final.csv "$1"/dada/tax_final.csv "$1"/dada/seqtab_final.rds "$1"/dada/tax_final.rds;
+
+# sh _submit.sh "$1" "$2" "$len";
 
 exit 0;
