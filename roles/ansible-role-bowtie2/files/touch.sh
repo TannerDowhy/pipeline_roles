b69350df1ca8d91da3e620d5cbdd2b0afc051677
Tanner Dowhy <tad638@mail.usask.ca>
2018-05-02 02:32:56 -0600

    fix wildcard

--- roles/ansible-role-bowtie2/files/touch.sh
+++ roles/ansible-role-bowtie2/files/touch.sh
@@ -5,7 +5,7 @@ for file in "$1"/*_R1_.fastq.gz;
 do
   inp="$file";
   file_name=${inp##*/};
-  base=${file_name%_R*.fastq.gz};
+  base=${file_name%_R};
   touch "$1"/bowtie2/output/"$base".fastq;
 done
 
--- roles/ansible-role-merging/files/touch.sh
+++ roles/ansible-role-merging/files/touch.sh
@@ -5,7 +5,7 @@ for file in "$1"/*_R1_.fastq.gz;
 do
   # inp="$file";
   file_name=${file##*/};
-  base=${file_name%_R*.fastq.gz};
+  base=${file_name%_R};
   touch "$1"/merging/output/"$base".extendedFrags.fastq;
 done
 
