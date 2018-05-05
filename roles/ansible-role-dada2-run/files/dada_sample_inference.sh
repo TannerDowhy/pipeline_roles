2a6b9ea8c3f2160ff1e044265a496937012127d7
Tanner Dowhy <tad638@mail.usask.ca>
2018-05-04 19:09:50 -0600

    dada2 script path

--- roles/ansible-role-dada2-run/files/dada_sample_inference.sh
+++ roles/ansible-role-dada2-run/files/dada_sample_inference.sh
@@ -6,6 +6,7 @@
 #SBATCH --nodes=8
 #SBATCH --tasks-per-node=32
 
-`module load r; Rscript $6/dada/dada_sample_inference.R $1 $2 $3 $4 $5`;
+`module load r`;
+`Rscript $6/dada/dada_sample_inference.R $1 $2 $3 $4 $5`;
 
 exit 0;
