82ae6367b87cd793d46fb65b8e39c9b0701c13f5
Tanner Dowhy <tad638@mail.usask.ca>
2018-05-08 00:10:20 -0600

    Spelling

--- roles/ansible-role-primer-removal/files/touch.sh
+++ roles/ansible-role-primer-removal/files/touch.sh
@@ -6,7 +6,7 @@ do
   inp="$file";
   file_name=${inp##*/};
   base=${file_name%fastq.gz};
-  touch $1/primer_removal/output/$base.cutadaptedfastq.gz;
+  touch $1/primer_removal/output/$base.cutadapted.fastq.gz;
 done
 
 exit 0;
