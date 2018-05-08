35b7d49dfd9a1af955a7bba42ece057b0cbf8af6
Tanner Dowhy <tad638@mail.usask.ca>
2018-05-07 18:35:40 -0600

    Rscript path

--- roles/ansible-role-dada2-run/files/dada_chimera_taxonomy.sh
+++ roles/ansible-role-dada2-run/files/dada_chimera_taxonomy.sh
@@ -8,6 +8,6 @@
 
 # module load r;
 # /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/r/3.4.3/bin/Rscript $8/dada/dada_chimera_taxonomy.R $1 $2 $3 $4 $5 $6 $7;
-/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/r/3.4.3/bin/Rscript `$8/dada/dada_chimera_taxonomy.R $1 $2 $3 $4 $5 $6 $7`;
+/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/r/3.4.3/bin/Rscript ${8}/dada/dada_chimera_taxonomy.R ${1 2 3 4 5 6 7};
 
 exit 0;
