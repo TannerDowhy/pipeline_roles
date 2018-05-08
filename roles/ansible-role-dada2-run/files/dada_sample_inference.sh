001ee1e8f4f05a97de1b1a70d7aa5ee97cbdae29
Tanner Dowhy <tad638@mail.usask.ca>
2018-05-07 19:47:38 -0600

    trying script run from directory

--- roles/ansible-role-dada2-run/files/dada_sample_inference.sh
+++ roles/ansible-role-dada2-run/files/dada_sample_inference.sh
@@ -8,6 +8,6 @@
 
 # `module load r`;
 # `/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/r/3.4.3/bin/Rscript $6/dada/dada_sample_inference.R $1 $2 $3 $4 $5`;
-/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/r/3.4.3/bin/Rscript ${6}/dada/dada_sample_inference.R ${1} ${2} ${3} ${4} ${5};
+/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/r/3.4.3/bin/Rscript dada_sample_inference.R ${1} ${2} ${3} ${4} ${5};
 
 exit 0;
