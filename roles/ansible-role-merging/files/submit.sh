a3623279610b93bdce90aa672f14c735aeafbe9f
Tanner Dowhy <tad638@mail.usask.ca>
2018-06-22 20:29:40 -0600

    debug

--- roles/ansible-role-merging/files/submit.sh
+++ roles/ansible-role-merging/files/submit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for file in "$1"/primer_removal/output/*_R1*.fastq.gz;
-    do sbatch --output=/dev/null --account="$2" --dependency=singleton "$1"/merging/merge.sh "$file" "$1";
+# for file in "$1"/primer_removal/output/*_R1*.fastq.gz;
+#     do sbatch --output=/dev/null --account="$2" --dependency=singleton "$1"/merging/merge.sh "$file" "$1";
+# done;
+for file in "$1"/*_R1*.fastq.gz;
+    do sbatch --output=/dev/null --account="$2" "$1"/merging/merge.sh "$file" "$1";
 done;
 
 exit 0;
