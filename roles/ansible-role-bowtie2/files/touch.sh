55b1698b10c9205935cf6031e1664a744a14b04a
Tanner Dowhy <tad638@mail.usask.ca>
2018-07-09 22:23:24 -0600

    Non-merging

--- roles/ansible-role-bowtie2/files/touch.sh
+++ roles/ansible-role-bowtie2/files/touch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-for file in "$1"/*_R1*.fastq.gz
+# for file in "$1"/*_R1*.fastq.gz
+for file in "$1"/*.fastq.gz
 do
   inp="$file";
   file_name=${inp##*/};
   base=${file_name%_R1*};
-  touch $1/bowtie2/output/$base.fastq;
+  touch $1/bowtie2/output/$file_name.fastq;
 done
 
 exit 0;
