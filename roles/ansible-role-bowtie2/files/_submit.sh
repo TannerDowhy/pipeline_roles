bd604604693425d196f0611e2dae21f96dcbef6a
Tanner Dowhy <tad638@mail.usask.ca>
2018-08-29 18:09:36 -0600

    no merging

--- roles/ansible-role-bowtie2/files/_submit.sh
+++ roles/ansible-role-bowtie2/files/_submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for file in "$1"/merging/output/*extendedFrags.fastq;
-    do sbatch --output=/dev/null --account="$3" --dependency=singleton "$1"/bowtie2/bowtie2.sh "$file" "$1";
-done;
-
-# for file in "$1"/primer_removal/output/*.fastq.gz;
+# for file in "$1"/merging/output/*extendedFrags.fastq;
 #     do sbatch --output=/dev/null --account="$3" --dependency=singleton "$1"/bowtie2/bowtie2.sh "$file" "$1";
 # done;
+
+for file in "$1"/primer_removal/output/*.fastq.gz;
+    do sbatch --output=/dev/null --account="$3" --dependency=singleton "$1"/bowtie2/bowtie2.sh "$file" "$1";
+done;
 
 exit 0;
