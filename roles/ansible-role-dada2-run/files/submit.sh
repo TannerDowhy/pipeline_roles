de70a429fb0b2a3b6723c8cc5f6b3aee4e6af6ec
Tanner Dowhy <tad638@mail.usask.ca>
2018-09-19 21:49:28 -0600

    poster

--- roles/ansible-role-dada2-run/files/submit.sh
+++ roles/ansible-role-dada2-run/files/submit.sh
@@ -21,7 +21,17 @@ len=`python $1/dada/read_length.py -f $1/dada/hist.txt`;
 
 `sbatch --account=$2 --output=$1/dada/sample_inference.out $1/dada/dada_sample_inference.sh $1/bowtie2/output $len $1/dada/plot.png $1/dada/seq_tab.csv $1/dada/seq_tab.rds`;
 
-`sbatch --account=$2 --dependency=singleton --output=$1/dada/chimera_taxonomy.out $1/dada/dada_chimera_taxonomy.sh $1/dada/seq_tab.rds $1/dada/seqtab_final.csv $1/dada/tax_final.csv $1/dada/seqtab_final.rds $1/dada/tax_final.rds $1/dada/tax_gg.csv $1/dada/tax_gg.rds`;
+while true
+do
+    command=`squeue -u $1 | wc -l`
+    if [ $command = "1" ];
+    then
+        break;
+    fi
+    sleep 10;
+done
+
+`sbatch --account=$2 --output=$1/dada/chimera_taxonomy.out $1/dada/dada_chimera_taxonomy.sh $1/dada/seq_tab.rds $1/dada/seqtab_final.csv $1/dada/tax_final.csv $1/dada/seqtab_final.rds $1/dada/tax_final.rds $1/dada/tax_gg.csv $1/dada/tax_gg.rds`;
 
 # sh _submit.sh "$1" "$2" "$len";
 
