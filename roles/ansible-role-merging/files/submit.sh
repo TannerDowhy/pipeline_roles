4c7cca403fd75f74cd836fe9737e4ea3bca4e58b
Tanner Dowhy <tad638@mail.usask.ca>
2018-09-19 21:57:31 -0600

    typo

--- roles/ansible-role-merging/files/submit.sh
+++ roles/ansible-role-merging/files/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in "$1"*_R1*.fastq.gz;
+for file in "$1"/*_R1*.fastq.gz;
     do sbatch --output=/dev/null --account="$2" --dependency=singleton "$1"/merging/merge.sh "$file" "$1";
 done;
 # for file in "$1"/primer_removal/output/*_R1*.fastq.gz;
